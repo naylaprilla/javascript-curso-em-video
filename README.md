b21446aa7a6164543ee7bd4dc4278d3c6b45cbfe
naylaprilla <nayla.priscila@gmail.com>
2023-06-27 10:06:14 -0300

    For e Debbug

--- README.md
+++ README.md
@@ -20,3 +20,9 @@ Anotações:
     Estruturas de controle de repeticao com teste lógico no inicio(while), estrutura de repetição com teste lógico no final(do while) e estrutura de repetição com controle(for). 
     Condição / laço / repetição
     while em inglês é o mesmo que enquanto
+
+    Como usar o modo Debug do VsCode:
+     1- Clique em Run and Debug
+     2- Crie um breackpoint(ponto de inicio) na frente da variável que você quer inspecionar
+     3- Com a tecla f12 é possível dar um passo a frente e assim acompanhar cada passo com a setinha amarela
+     Em baixo da tela, na aba Debug Console, também é possível acompanhar o passo a passo escrito da execussão da instrução do programa.
--- aula_14/ambiente.js
+++ aula_14/ambiente.js
@@ -1,16 +1,34 @@
-//Estrutura de repetição com controle - como reescrever o while com o for - *mais utilizado
+//Estrutura de repetição com controle - como reescrever o while com o for - *mais utilizado no dia a dia
 /*   
     for(inicio; testeLogico; incremento) {
         
     }
     
-*/
-var contador = 1
-while (c<=10) {
-    console.log('Olá!')
-    contador++
-} 
+    var contador = 1
+    while (contador<=10) {
+        console.log('Olá!')
+        contador++
+    } 
 
-for(var contador=1; contador<=10; contador++){
-    console.log('Olá!')
+    ou poderia ser escrito assim: 
+
+    var contador = 1
+    do {
+        console.log('Olá!')
+        contador++
+    } while(contador<=10) {
+
+    }
+
+    for(var contador=1; contador<=10; contador++){
+        console.log(`Passo ${contador}`)
+    }
+*/
+
+console.log("Vai começar... ")
+for(var contador = 1; contador <= 4; contador++) {
+    console.log(contador)
 }
+console.log("FIM!")
+
+// Usamos o modo debug e breackpoints para ver o funcionamento do for - repetição 
