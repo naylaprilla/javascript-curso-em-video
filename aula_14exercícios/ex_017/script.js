08a2a9468cf2ff57a1bbea3037133caa03e775dd
naylaprilla <nayla.priscila@gmail.com>
2023-07-03 11:28:04 -0300

    Uso do Select e Option

--- aula_14exercícios/ex_017/script.js
+++ aula_14exercícios/ex_017/script.js
@@ -1,5 +1,22 @@
 function gerarTabuada(event) {
     event.preventDefault();
 
+    const numeroDigitado = document.querySelector('#numero');
+    const selectTabuada = document.querySelector('#selTab');
+    
+    if (numero.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let numero = Number(numeroDigitado.value);
+        let contador = 1
+        selectTabuada.innerHTML = ''
+
+         for( let contador = 1; contador <= 10; contador++){
+            let item = document.createElement('option')  
+            item.text = `${numero} x ${contador} = ${numero*contador}`
+            item.value = `selectTabuada${contador}` 
+            selectTabuada.appendChild(item)    
+        }
+    }
 
 }
