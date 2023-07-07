29c7c2e2610eeacbe74252a1ecec0b570afbc54e
naylaprilla <nayla.priscila@gmail.com>
2023-07-06 21:09:14 -0300

    Desafio - mão na massa 01

---
+++ Desafios/desafioDasMensagens/script.js
@@ -0,0 +1,18 @@
+function mensagem1(event) {
+    event.preventDefault();
+    
+    const botao01 = document.querySelector('btn01')
+    alert('Você clicou no primeiro botão!')
+}
+function mensagem2(event) {
+    event.preventDefault();
+    
+    const botao01 = document.querySelector('btn02')
+    alert('Você clicou no segundo botão!')
+}
+function mensagem3(event) {
+    event.preventDefault();
+    
+    const botao01 = document.querySelector('btn03')
+    alert('Você clicou no terceiro botão!')
+}
